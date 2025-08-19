5d881dc3a1551f0c9bca749b900fd7d5a9d33302
yessa-rosa <yessarosa43@gmail.com>
2025-08-19 16:49:48 -0300

    Javascript aula um

---
+++ Aula-01/script.js
@@ -0,0 +1,54 @@
+//comentário de uma linha só
+
+/*
+   comenta
+   várias
+   linhas
+*/
+
+//console.log = escreva do portugol
+console.log("UMA MENSAGEM TOP AQUI") // Tá comentado
+
+// Cria uma janela de alerta na página
+// alert("TESTA AI E VE")
+
+// VAr cria uma variável na tela
+var nome = "Yessa"
+console.log("O nome é:" + nome)
+
+// Cadeia = String
+var texto = "Rosa"
+console.log(typeof(texto))
+
+// inteiro = number, int
+var numero = 2008
+console.log(typeof(inteiro))
+
+// real = number, float
+var VouF = true
+console.log(typeof(quebrado))
+
+// lógico = bool
+var nulo = null
+console.log(typeof(VouF))
+
+// Valor nulo
+var indefinido = null
+console.log(typeof(nulo))
+
+// Variável sem valor iniciado
+var indefinido = undefined
+console.log(typeof(indefinido))
+
+// Operadores sritiméticos
+// +,-,*,/,%
+
+var a = 10
+var b = 3
+
+console.log("Soma", a + b);
+console.log("Subtracão", a - b);
+console.log("Multiplicação", a * b);
+console.log("Dvisão", a / b);
+console.log("Módulo", a % b);
+
