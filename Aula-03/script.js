3c179ba593349f72d50628bcd52e3a991f0a96a6
yessa-rosa <yessarosa43@gmail.com>
2025-08-21 17:11:30 -0300

    aula3

---
+++ Aula-03/script.js
@@ -0,0 +1,25 @@
+console.log("BOM DIA!");
+//Laços de repetição
+
+//PARA = FOR
+for(var i = 0; i<5; i++){
+    console.log ("Espero que esteja bem!");
+    console.log("Espero que esteja bem!")
+}
+console.log("Se não, melhore")
+
+//ENQUANTO = while
+var contagem
+while (contagem < 10){
+    console.log("Estou repetindo infinitamente");
+    contagem++
+}
+
+//Exercicio 1
+var numeroEscolha = prompt("Digite um numero:")
+
+for(var i = 0; i <= numeroEscolha; i++){
+    console.log("Contagem:" , i)
+}
+
+//Exercicio 2
